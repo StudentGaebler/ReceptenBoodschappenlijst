486c61ca0185f7e6e133d13c9765951866d6d0fc
StudentGaebler <m.gabler@st.hanze.nl>
2021-12-22 14:42:00 +0100

    Initial commit

---
+++ src/main/java/nl/miwgroningen/se/ch7/advanced/martijn/receptenBoodschappenlijst/ReceptenBoodschappenlijstApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se.ch7.advanced.martijn.receptenBoodschappenlijst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptenBoodschappenlijstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptenBoodschappenlijstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/se/ch7/advanced/martijn/receptenBoodschappenlijst/ReceptenBoodschappenlijstApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se.ch7.advanced.martijn.receptenBoodschappenlijst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptenBoodschappenlijstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
